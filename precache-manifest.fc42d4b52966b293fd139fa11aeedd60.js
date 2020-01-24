0f2e3e9f6cbdd65ca3a5689bc9daaf1c5bbb8e24
Nataliia Korneiko <korneyko_ns@ukr.net>
2020-01-24 22:12:45 +0200

    Updates

---
+++ precache-manifest.fc42d4b52966b293fd139fa11aeedd60.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d3a3fdde69de2b129934cc2daa9fcd",
+    "url": "/project-shoe-store/index.html"
+  },
+  {
+    "revision": "6aed25fc92a84c6879f6",
+    "url": "/project-shoe-store/static/css/main.cf418aa9.chunk.css"
+  },
+  {
+    "revision": "7a4991435dcde5ad1650",
+    "url": "/project-shoe-store/static/js/2.5ba235df.chunk.js"
+  },
+  {
+    "revision": "ed7f03a505e0a279ce0e425bcbc1eeed",
+    "url": "/project-shoe-store/static/js/2.5ba235df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6aed25fc92a84c6879f6",
+    "url": "/project-shoe-store/static/js/main.e216186c.chunk.js"
+  },
+  {
+    "revision": "a51635a0b5b82378b8d1",
+    "url": "/project-shoe-store/static/js/runtime-main.e4e94cbb.js"
+  },
+  {
+    "revision": "c4fb96908a77d81fd7eaafd73db52814",
+    "url": "/project-shoe-store/static/media/best-seller-01.c4fb9690.jpg"
+  },
+  {
+    "revision": "6fb1b5623e528e27c18658fecf5ee0ee",
+    "url": "/project-shoe-store/static/media/montserrat-v14-latin-600.6fb1b562.woff2"
+  },
+  {
+    "revision": "7c839d15a6f54e7025ba8c0c4b333e8f",
+    "url": "/project-shoe-store/static/media/montserrat-v14-latin-600.7c839d15.woff"
+  },
+  {
+    "revision": "39d93cf678c740f9f6b2b1cfde34bee3",
+    "url": "/project-shoe-store/static/media/montserrat-v14-latin-700.39d93cf6.woff2"
+  },
+  {
+    "revision": "80f10bd382f0df1cd650fec59f3c9394",
+    "url": "/project-shoe-store/static/media/montserrat-v14-latin-700.80f10bd3.woff"
+  },
+  {
+    "revision": "8102c4838f9e3d08dad644290a9cb701",
+    "url": "/project-shoe-store/static/media/montserrat-v14-latin-regular.8102c483.woff"
+  },
+  {
+    "revision": "bc3aa95dca08f5fee5291e34959c27bc",
+    "url": "/project-shoe-store/static/media/montserrat-v14-latin-regular.bc3aa95d.woff2"
+  },
+  {
+    "revision": "72bed90f6f1b5aeee9ec5a51a5e19dff",
+    "url": "/project-shoe-store/static/media/photo-01-left.72bed90f.jpg"
+  },
+  {
+    "revision": "62f5d5069fda564bfbd7171db299a96f",
+    "url": "/project-shoe-store/static/media/photo-01-right.62f5d506.jpg"
+  },
+  {
+    "revision": "068ec18eac9667e9cc231e3b268a16ba",
+    "url": "/project-shoe-store/static/media/photo-02-left.068ec18e.jpg"
+  },
+  {
+    "revision": "ca52b66676ccd22d37e55441bc9828bd",
+    "url": "/project-shoe-store/static/media/photo-02-right.ca52b666.jpg"
+  },
+  {
+    "revision": "9688b3605bad98d57e5d8cb4722b0b0f",
+    "url": "/project-shoe-store/static/media/photo-03-left.9688b360.jpg"
+  },
+  {
+    "revision": "aad7ee7fc582ff8a38d1a77fc9637804",
+    "url": "/project-shoe-store/static/media/photo-03-right.aad7ee7f.jpg"
+  },
+  {
+    "revision": "5b059007201face46a41e4a1ab8f5ffc",
+    "url": "/project-shoe-store/static/media/photo-04-left.5b059007.jpg"
+  },
+  {
+    "revision": "7ae1ecf89dcb7ea363c7d727d243d558",
+    "url": "/project-shoe-store/static/media/photo-04-right.7ae1ecf8.jpg"
+  },
+  {
+    "revision": "ca8c6f0a7383b5fa9c2097135db06281",
+    "url": "/project-shoe-store/static/media/photo-05-left.ca8c6f0a.jpg"
+  },
+  {
+    "revision": "8086fe24a5ded0c9c7973244fc0f026d",
+    "url": "/project-shoe-store/static/media/photo-05-right.8086fe24.jpg"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/project-shoe-store/static/media/roboto-v20-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/project-shoe-store/static/media/roboto-v20-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/project-shoe-store/static/media/roboto-v20-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/project-shoe-store/static/media/roboto-v20-latin-500.87284894.woff"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/project-shoe-store/static/media/roboto-v20-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/project-shoe-store/static/media/roboto-v20-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/project-shoe-store/static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/project-shoe-store/static/media/roboto-v20-latin-regular.60fa3c06.woff"
+  }
+]);
